37e5fa5d41e78359ded4f1ce9a14a7b2b502d136
Alina Moskalenko <alina.kravchenko@ukr.net>
2019-07-07 20:28:07 +0300

    Added content to home page (v.1.0)

--- src/components/pages/home-page/home-page.js
+++ src/components/pages/home-page/home-page.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import chef from './img/chef_photo.jpg';
+import data from './chef_description';
 
 import styles from './home-page.module.scss';
 
 const HomePage = () => {
    return (
-     <div className={styles.homePage}>This is Home page</div>
+    <div className={styles.homePage}>
+      <div className={styles.categoryBanner}></div>
+      <div className={styles.aboutChef}>
+        <div className={styles.photo}>
+          <img src={chef} alt="Chef" />
+        </div>
+        <div className={styles.description}>
+          <p>{ data.paragraph1 }</p>
+          <p>{ data.paragraph2 }</p>
+          <p>{ data.paragraph3 }</p>
+          <p>{ data.paragraph4 }</p>
+        </div>
+      </div>
+    </div>
    );
 };
 
